27a1890ccc44963b4f6d3f9f18393f9f8d53cf1a
Onur <onur.yilmazer1@gmail.com>
2018-03-15 22:59:16 +0300

    adjustable canvas resolution

--- canvas.js
+++ canvas.js
@@ -1,33 +1,24 @@
 document.addEventListener("DOMContentLoaded", main);
 
+
+let container;
+let canvas;
+let squareCount;
+
 function main(event) {
-	let container = document.getElementById("mainContainer");
-	let canvas = document.getElementById("canvas");
+	container = document.getElementById("mainContainer");
+	canvas = document.getElementById("canvas");
+	squareCount = 64;
 
-	let squareCount = 64;
-
-	let canvasWidth = parseFloat(window.getComputedStyle(canvas).getPropertyValue("width"));
-	let canvasHeight = parseFloat(window.getComputedStyle(canvas).getPropertyValue("height"));
-	let canvasArea = canvasWidth * canvasHeight;
-	let squareArea = canvasArea / squareCount;
-	let squareSideLength = Math.sqrt(squareArea);
-	console.log(squareSideLength); //75
-
-	for (let i =0; i < squareCount; i++) {
-		let square = document.createElement("div");
-		square.classList.add("square");
-		square.style.width = squareSideLength + "px";
-		square.style.height = squareSideLength + "px";
-		canvas.appendChild(square);
-	}
-
+	document.getElementById("size").value = squareCount;
+	updateCanvas(squareCount);
 
 	canvas.addEventListener("mousedown", function(event) {mouseDown = true; event.preventDefault(); color(event);});
 	canvas.addEventListener("mouseover", color);
 	document.addEventListener("mouseup", function() {mouseDown = false;});
 
 	document.getElementById("size").addEventListener("change", sliderChange);
-	
+	document.getElementById("reset").addEventListener("click", function() {updateCanvas(squareCount);});
 	
 }
 
@@ -50,5 +41,25 @@ function sliderChange(event) {
 	}
 
 	event.target.value = valueToStick;
+	updateCanvas(event.target.value);
 	console.log(event.target.value);
+}
+
+function updateCanvas(squareCount) {
+	let canvasWidth = parseFloat(window.getComputedStyle(canvas).getPropertyValue("width"));
+	let canvasHeight = parseFloat(window.getComputedStyle(canvas).getPropertyValue("height"));
+	let canvasArea = canvasWidth * canvasHeight;
+	let squareArea = canvasArea / squareCount;
+	let squareSideLength = Math.sqrt(squareArea);
+	console.log(squareSideLength); //75
+
+	canvas.innerHTML = "";
+
+	for (let i =0; i < squareCount; i++) {
+		let square = document.createElement("div");
+		square.classList.add("square");
+		square.style.width = squareSideLength + "px";
+		square.style.height = squareSideLength + "px";
+		canvas.appendChild(square);
+	}
 }
